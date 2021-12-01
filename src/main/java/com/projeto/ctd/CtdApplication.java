e04e0c4ebb0f0d71bc8618a05eab7f8397179cb0
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2021-12-01 09:48:01 -0300

    Initial commit

---
+++ src/main/java/com/projeto/ctd/CtdApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ctd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtdApplication.class, args);
+	}
+
+}
