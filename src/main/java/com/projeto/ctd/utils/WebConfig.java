dcea2fb0e106c8fdea637ee687fcddad4315dedc
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2021-12-03 15:41:00 -0300

    permitindo o cors

--- src/main/java/com/projeto/ctd/utils/WebConfig.java
+++ src/main/java/com/projeto/ctd/utils/WebConfig.java
@@ -5,13 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://imagens-react.vercel.app");
-        registry.addMapping("/**").allowedOrigins("https://ctd-checkpoint-integrador-frontend-40q610ztp-hiimgui.vercel.app");
+        registry.addMapping("/**").allowedOrigins("https://ctd-checkpoint-integrador-frontend-40q610ztp-hiimgui.vercel.app","https://imagens-react.vercel.app" );
     }
 }
