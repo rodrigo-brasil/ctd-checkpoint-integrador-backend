6cd6adc2f831fe51e67efb87d0281c148fc83de0
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2021-12-03 15:11:33 -0300

    permitindo o cors

--- src/main/java/com/projeto/ctd/utils/WebConfig.java
+++ src/main/java/com/projeto/ctd/utils/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://imagens-react.vercel.app");
+//        registry.addMapping("/**").allowedOrigins("https://imagens-react.vercel.app");
     }
 }
