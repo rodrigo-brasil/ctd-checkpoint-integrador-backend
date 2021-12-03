32984dca09e3c62afd3c941897e3435226c9c69f
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2021-12-03 15:25:39 -0300

    permitindo o cors

--- src/main/java/com/projeto/ctd/utils/WebConfig.java
+++ src/main/java/com/projeto/ctd/utils/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("https://imagens-react.vercel.app");
+        registry.addMapping("/**").allowedOrigins("https://imagens-react.vercel.app");
+        registry.addMapping("/**").allowedOrigins("https://ctd-checkpoint-integrador-frontend-40q610ztp-hiimgui.vercel.app");
     }
 }
