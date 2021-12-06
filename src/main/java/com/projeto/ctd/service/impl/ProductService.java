3d14d1a7619a1f442600e5b3ecc70cdbed635cba
Rodrigo S Brasil <maic_rodrigo@hotmail.com>
2021-12-06 12:05:35 -0300

    Add ResourceNotFoundException e algumas melhorias

--- src/main/java/com/projeto/ctd/service/impl/ProductService.java
+++ src/main/java/com/projeto/ctd/service/impl/ProductService.java
@@ -9,7 +9,6 @@ import com.projeto.ctd.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
--- src/main/java/com/projeto/ctd/utils/WebConfig.java
+++ src/main/java/com/projeto/ctd/utils/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*" );
+        registry.addMapping("/**").allowedOrigins("*" ).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 //        registry.addMapping("/**").allowedOrigins("https://ctd-checkpoint-integrador-frontend-40q610ztp-hiimgui.vercel.app");
     }
 }
